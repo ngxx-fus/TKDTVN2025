2c44783f85434d4cb5cef0028afece7fbd4d7821
Nguyễn Thanh Phú <msnp@outlook.com.vn>
2025-11-05 10:45:52 +0000

    add Task03

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 void Task01(void* pv){
     __entry("Task01()");
-    
+    /// Task01 : Blink LED with period 200 tick
     while(1){
         GPIO.out_w1tc = __mask32(HM_LED0_PIN);
         vTaskDelay(100);
@@ -15,21 +15,63 @@ void Task01(void* pv){
 
 void Task02(void* pv){
     __entry("Task02()");
-    
-
+    /// Task02 : Blink LED with period 700 tick
     while(1){
         GPIO.out_w1tc = __mask32(HM_LED1_PIN);
-        vTaskDelay(300);
+        vTaskDelay(350);
         GPIO.out_w1ts = __mask32(HM_LED1_PIN);
-        vTaskDelay(300);
+        vTaskDelay(350);
     }
     __exit("Task02()");
 }
 
-void setup(){
-    __entry("setup()");
-    Serial.begin(115200);
+void Task03(void *pv)
+{
+    __entry("Task03()");
 
+    MPU6050 mpu;
+
+    // --- Init I2C ---
+    Wire.begin();
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+
+    // --- Init MPU6050 ---
+    mpu.initialize();
+    if (!mpu.testConnection()) {
+        __log("[Task03] "  "MPU6050 connection failed!");
+        vTaskDelete(NULL); // Dừng task nếu cảm biến không phản hồi
+    } else {
+        __log("[Task03] "  "MPU6050 connected successfully.");
+    }
+
+    // Data variables
+    int16_t ax, ay, az;
+    int16_t gx, gy, gz;
+
+    // Main loop
+    while (1) {
+        mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+        // Print onto Serial / Log
+        __log("[Task03] " "A[x:%d y:%d z:%d] G[x:%d y:%d z:%d]", ax, ay, az, gx, gy, gz);
+
+        // Delay 750 ms
+        vTaskDelay(pdMS_TO_TICKS(350));
+    }
+
+    __exit("Task03()");
+    vTaskDelete(NULL);
+}
+
+void setup(){
+    /// For serial log | Baudrate: 115200
+    Serial.begin(115200);
+    /// For semaphore log
+    semaphoreLogInit();
+    /// Other set-up
+    __entry("setup()");
+
+    /// Set-up LED for Task01 and Task02
     gpio_config_t outPin = {
         .pin_bit_mask = __masks64(HM_LED0_PIN, HM_LED1_PIN),
         .mode = GPIO_MODE_OUTPUT,
@@ -39,16 +81,18 @@ void setup(){
     };
     gpio_config(&outPin);
 
+    /// Add Task01 and Task02 and Task03
     __log("[+] Add Task01");
     xTaskCreate(Task01, "Task01", 2048, NULL, 1, NULL);
-    __log("[+] Add Task01");
+    __log("[+] Add Task02");
     xTaskCreate(Task02, "Task02", 2048, NULL, 1, NULL);
-
+    __log("[+] Add Task03");
+    xTaskCreate(Task03, "Task03", 2048, NULL, 1, NULL);
 
     __exit("setup()");
 }
 
 void loop(){
-    __log("running...");
-    delay(1000);
+    /// Infinity lock :>
+    vTaskDelay(portMAX_DELAY);
 }
