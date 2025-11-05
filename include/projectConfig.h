af36b625bc92527c9c2967e2212fdc562abf438b
Nguyễn Thanh Phú <msnp@outlook.com.vn>
2025-11-05 09:49:12 +0000

    add esp-idf function wrapper

--- include/projectConfig.h
+++ include/projectConfig.h
@@ -3,15 +3,18 @@
 
 #include "stdint.h"
 
-#define CB_LOG_L0_EN 1
-#define CB_LOG_L1_EN 0
-#define CB_LOG_L2_EN 0
-#define CB_LOG_ERR_EN 1
-#define CB_LOG_L0_ENTRY 1
-#define CB_LOG_L0_EXIT 1
-#define CB_LOG_L1_ENTRY 0
-#define CB_LOG_L1_EXIT 0
-#define CB_LOG_L2_ENTRY 0
-#define CB_LOG_L2_EXIT 0
+#define SYS_LOG_L0_EN           1
+#define SYS_LOG_L1_EN           0
+#define SYS_LOG_L2_EN           0
+#define SYS_LOG_ERR_EN          1
+#define SYS_LOG_L0_ENTRY        1
+#define SYS_LOG_L0_EXIT         1
+#define SYS_LOG_L1_ENTRY        0
+#define SYS_LOG_L1_EXIT         0
+#define SYS_LOG_L2_ENTRY        0
+#define SYS_LOG_L2_EXIT         0
+
+#define HM_LED0_PIN             2     
+#define HM_LED1_PIN             0     
 
 #endif
