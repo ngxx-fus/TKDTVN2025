ab03108717d7ca670d609d5e2d8ba0d064d00c02
Nguyễn Thanh Phú <msnp@outlook.com.vn>
2025-11-05 09:49:46 +0000

    changed

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
 #include "../include/localHelperAndUtil.h"
 
+void Task01(void* pv){
+    __entry("Task01()");
+    
+    while(1){
+
+        vTaskDelay(1);
+    }
+
+    __exit("Task01()");
+}
+
+void Task02(void* pv){
+    
+
+    while(1){
+        vTaskDelay(1);
+    }
+}
+
 void setup(){
     __entry("setup()");
-
     Serial.begin(115200);
+
+    gpio_config_t outPin = {
+        .pin_bit_mask = __masks64(HM_LED0_PIN, HM_LED1_PIN),
+        .mode = GPIO_MODE_OUTPUT,
+        .pull_up_en = GPIO_PULLUP_DISABLE,
+        .pull_down_en = GPIO_PULLDOWN_DISABLE,
+        .intr_type = GPIO_INTR_DISABLE,
+    };
+    gpio_config(&outPin);
+
 
     __exit("setup()");
 }
