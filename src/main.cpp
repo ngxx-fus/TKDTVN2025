47ea2473f3236445913cd0b072e1bff128df2c3e
Nguyễn Thanh Phú <msnp@outlook.com.vn>
2025-11-05 09:18:42 +0000

    changed

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,3 @@ void loop(){
     __log("running...");
     delay(1000);
 }
-
-
-/*
-Note for using terminal:(Only for HoangAnh's PC)
-- Build:    C:\Users\Admin\.platformio\penv\Scripts\pio.exe run
-- Upload:   C:\Users\Admin\.platformio\penv\Scripts\pio.exe run -t upload
-- Upload:   C:\Users\Admin\.platformio\penv\Scripts\pio.exe run --target monitor
-
-For shorter command, you need add the `C:\Users\Admin\.platformio\penv\Scripts` to environment.
-*/
